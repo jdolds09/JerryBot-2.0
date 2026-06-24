ba103dce8a3466e5597f23fc4b559e822881c270
Jerry Olds <jdolds97@att.net>
2026-06-24 16:11:39 -0500

    Trying play command with cookies

--- Commands/Music/Play.py
+++ Commands/Music/Play.py
@@ -16,7 +16,8 @@ YDL_OPTIONS = {'format': 'bestaudio/best',
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0'}
+    'source_address': '0.0.0.0',
+    'cookiefile': 'cookies.txt'}
 FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",'options' : '-vn -c:a pcm_s16le -b:a 192k'}
 ffmpeg_path = os.path.join(os.getcwd(), "ffmpeg", "ffmpeg.exe")
 
@@ -83,19 +84,12 @@ class Play(commands.Cog):
         with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
             try:
                 url = None
-                await ctx.send("fuck")
                 info_dictionary = ydl.extract_info(f"ytsearch:{args}", download=False)
-                await ctx.send("fuck")
                 if 'entries' in info_dictionary:
-                    await ctx.send("fuck")
                     info = info_dictionary['entries'][0]
-                    await ctx.send("fuck")
                     titles.append(info["title"])
-                    await ctx.send("fuck")
                     urls.append(info["webpage_url"])
-                    await ctx.send("fuck")
                     url = info['url']
-                    await ctx.send("fuck")
             except Exception as e:
                 await ctx.send(f"{e}")
                 return await ctx.send("An error occurred while searching for your query. Please try again.")
